c9d56e1887050eb67377d9be0b387ced5b7fa418
profinforobsoncarlos <profrobsoncarlos2024@gmail.com>
2024-04-25 13:21:11 -0400

    at

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+//CONFIGURAÇÕES DE CREDENCIAIS
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "aula2404";
+
+//CONEXÃO COM NOSSO BANCO DE DADOS
+$conn = new mysqli($servidor, $usuario, $senha, $banco);
+
+//VERIFICANDO A CONEXÃO
+if ($conn->connect_error) {
+    die("Falha ao se comunicar com o Banco de Dados:".$conn->connect_error);
+}
+?>
